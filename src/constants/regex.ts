42cbabfca3a99a174ddccccb51c229620084d755
Viet Pham <viet.pham@asnet.com.vn>
2026-02-03 09:59:16 +0700

    #333 Chore: Update schema with build-in validation

--- src/constants/regex.ts
+++ src/constants/regex.ts
@@ -2,3 +2,4 @@ export const UPPERCASE_REGEX = /[A-Z]/;
 export const LOWERCASE_REGEX = /[a-z]/;
 export const SPECIAL_CHAR_REGEX = /[!@#$%^&*(),.?":{}|<>\[\]\\\/_+\-=~`]/;
 export const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+export const PHONE_NUMBER_REGEX = /^\+?[1-9]\d{1,14}$/;
