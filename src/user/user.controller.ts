b9bb44595677ab80285f50b2a0048bcafdfee44e
viet.pham <viet.pham@asnet.com.vn>
2026-07-17 17:50:55 +0700

    feat: add RolesGuard for admin role protection and implement observable stream for user data

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Res, UseGuards } from '@nestjs/common';
 import { UserService } from './users.service';
+import { RolesGuard } from 'src/guard/authorize';
+import { createReadStream } from 'fs';
+import { join } from 'path';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
+@UseGuards(RolesGuard)
 @Controller('users')
 export class UserController {
-  constructor(private usersService: UserService) {}
+  constructor(private usersService: UserService) { }
 
   @Get()
   findAll(): string {
@@ -13,5 +19,18 @@ export class UserController {
   @Get('/:userId')
   fetchUserDetails(@Param('userId') userId: string): string {
     return this.usersService.fetchUserDetails(userId);
+  }
+
+  @Get('observable-stream')
+  streamWithObservable(): Observable<any> {
+    const filePath = join(__dirname, 'large-dataset.csv');
+    const stream = createReadStream(filePath);
+    return new Observable(observer => {
+      stream.on('data', (chunk) => observer.next(chunk));
+      stream.on('error', (err) => observer.error(err));
+      stream.on('end', () => observer.complete());
+    }).pipe(
+      map(chunk => ({ data: chunk.toString() })),
+    );
   }
 }
