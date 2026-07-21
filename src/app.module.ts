97e2ea828ccd64304af59d9a31dee0080e59e953
viet.pham <viet.pham@asnet.com.vn>
2026-07-21 09:22:37 +0700

    feat: add socket.io dependency and integrate AppGateway into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/users.module';
 import { AuthModule } from './auth/auth.module';
 import { LogModule } from './log/log.module';
+import { AppGateway } from './socket';
 
 @Module({
-  imports: [UserModule, AuthModule, LogModule],
+  imports: [UserModule, AuthModule, LogModule, AppGateway],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -21,16 +21,4 @@ export class UserController {
     return this.usersService.fetchUserDetails(userId);
   }
 
-  @Get('observable-stream')
-  streamWithObservable(): Observable<any> {
-    const filePath = join(__dirname, 'large-dataset.csv');
-    const stream = createReadStream(filePath);
-    return new Observable(observer => {
-      stream.on('data', (chunk) => observer.next(chunk));
-      stream.on('error', (err) => observer.error(err));
-      stream.on('end', () => observer.complete());
-    }).pipe(
-      map(chunk => ({ data: chunk.toString() })),
-    );
-  }
 }
